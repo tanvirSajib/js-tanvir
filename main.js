c188512a0cc3d811f330d3efe452e23aa11c8f6e
TanvirSajib <tanvir.sajib.programmer@gmail.com>
2025-01-30 10:49:49 +0600

    To-Do List js (add task working)

---
+++ main.js
@@ -0,0 +1,29 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const taskInput = document.getElementById('taskInput');
+    const addTaskBtn = document.getElementById('addTaskBtn');
+    const taskList = document.getElementById('taskList');
+
+    addTaskBtn.addEventListener('click', function() {
+        const taskText = taskInput.value.trim();
+        if (taskText !== '') {
+            addTask(taskText);
+            taskInput.value = '';
+        }
+    });
+
+   
+
+    function addTask(taskText) {
+        const li = document.createElement('li');
+        li.textContent = taskText;
+
+        const deleteBtn = document.createElement('button');
+        deleteBtn.textContent = 'Delete';
+        deleteBtn.addEventListener('click', function() {
+            taskList.removeChild(li);
+        });
+
+        li.appendChild(deleteBtn);
+        taskList.appendChild(li);
+    }
+});
